f7b7acc1e2330ba6633bb17a0f6c2ea59154853e
coderbeltran <coder.beltran@gmail.com>
2024-03-03 15:59:40 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactosComponent } from './components/contactos/contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"inicio", component:HomeComponent},
+  {path:"nosotros", component:NosotrosComponent},
+  {path:"servicios", component:ServiciosComponent},
+  {path:"contactos", component:ContactosComponent},
+  {path:"", redirectTo:"/inicio", pathMatch:"full"},
+  {path:"**", component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactosComponent } from './components/contactos/contactos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    ContactosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
